19f80866cdea1c7a0eb350d31844810e64ff5d25
xdong12 <37921274+xdong12@users.noreply.github.com>
2020-11-27 14:27:39 +0800

    Create excute.py

---
+++ excute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Time: 2020/11/27 14:21
+# Author: xd
+
+import os
+command = "pip install -r  ./requirements.txt && cd ./employment/ && python run.py"
+os.system(command)
